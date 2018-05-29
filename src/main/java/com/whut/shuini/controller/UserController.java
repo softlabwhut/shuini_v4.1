8bfb5093cbb2fab61729a764481880352f84d169
danielwu927 <danielwu927@outlook.com>
2018-05-29 16:40:29 +0800

    加入注册页面

--- src/main/java/com/whut/shuini/controller/UserController.java
+++ src/main/java/com/whut/shuini/controller/UserController.java
@@ -5,7 +5,6 @@ import com.whut.shuini.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +13,6 @@ public class UserController {
     UserService userService;
 
     @RequestMapping(value = "/userByName")
-    @ResponseBody
     public Result userByName(@RequestParam("username") String username){
         Result result=new Result();
         result.setStatus(1);
